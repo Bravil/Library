16c4b2af02040accd072b40295c00595944fe0fa
Anni Paakala <anni.paakala@gmail.com>
2012-02-17 18:58:08 +0200

    lesson 5

---
+++ app/models/reservation.rb
@@ -0,0 +1,27 @@
+class Reservation < ActiveRecord::Base
+  belongs_to :reservation
+
+  before_validation :make_reserved, on: :create
+  
+  STATES = %w( free reserved )
+  
+  validates :book_id, presence: true
+  validates :state, inclusion: { in: STATES }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  validates :book_id, uniqueness: {
+          scope: :state,
+          message: 'book has been already reserved'
+        }, if: "state == 'reserved'"
+
+  def free
+    self.update_attributes({state: 'free'})
+  end
+
+  private
+    
+    def make_reserved
+      self.state = 'reserved'
+    end
+  
+end
